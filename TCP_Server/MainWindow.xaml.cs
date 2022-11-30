f9a8eec129b5529451fe0b90eec26c5ef8b01a0a
Berkan Çelik <celikberkan1@outlook.com>
2022-11-30 16:06:38 +0300

    Tcp_Server operation codes completed

--- TCP_Server/MainWindow.xaml.cs
+++ TCP_Server/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace TCP_Server
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() 
         {
             InitializeComponent();
         }
